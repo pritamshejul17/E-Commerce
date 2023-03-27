22ae59c3f0759a84a811708bf08c0b04ce6915e8
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-03-27 15:00:26 +0530

    Refactored some code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar(props) {
                 <li className = 'ml-6 py-9 px-2 border-b-2 border-transparent hover:border-b-2 hover:border-slate-600 hover:cursor-pointer' onMouseEnter={props.onHover} onMouseLeave ={props.offHover}><Link to = '/'>HOME & LIVING</Link></li>
                 <li className = 'ml-6 py-9 px-2 border-b-2 border-transparent hover:border-b-2 hover:border-slate-600 hover:cursor-pointer' onMouseEnter={props.onHover} onMouseLeave ={props.offHover}><Link to = '/'>KIDS</Link></li>
                 <li className = 'ml-4 flex justify-center grow'><input type = 'text' className= {`w-2/3 p-3 hover:bg-white hover:border-2 hover:border-blacks bg-stone-100 font-normal rounded-md`} placeholder = 'Search for products brands & more'></input></li>
-                <li className = 'ml-4 p-4 border-b-2 border-transparent hover:border-b-2 hover:border-slate-600' onMouseEnter = {props.isOn} onMouseLeave = {props.isOff}>
+                <li className = 'ml-4 p-4 mt-1.5 border-b-2 border-transparent hover:border-b-2 hover:border-slate-600' onMouseEnter = {props.isOn} onMouseLeave = {props.isOff}>
                     <Link to = '/'>
                         <div>
                             <img className = 'h-4 w-4 mt-2 ml-3' src={ profile }/>
