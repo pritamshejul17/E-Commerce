72fe2b7f252f9254d2d60a9a971989324f64a950
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-03-27 20:06:26 +0530

    Updated Product Details

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const sAll = ["s1.jpg", "s2.jpg", "s3.jpg", "s4.jpg", "s5.jpg", "s6.jpg", "s7.jpg", "s8.jpg"];
   const womenAll = ["l1.jpg", "l2.jpg", "l3.jpg", "l4.jpg", "l5.jpg", "l6.jpg", "l7.jpg", "l8.jpg"];
 
-  const wMeta = [{id : 1, info : "TRADITIONAL RAJASHTANI", price : "Rs 4999"}, {id : 2, info : "PUNJABI LEHNGA", price : "Rs 3999"}, {id : 3, info : "ANARKALI KURTA", price : "Rs 2999"}, {id : 4, info : "LEHNGA CHOLI", price : "Rs 6999"}, {id : 5, info : "HRX OVERSIZED", price : "Rs 3999"}, {id : 6, info : "ANARKALI KURTI", price : "Rs 4999"}, {id : 7, info : "TRADITIONAL RAJASHTANI", price : "Rs 3999"}, {id : 8, info : "TRADITIONAL INDIAN STYLE", price : "Rs 5999"}];
+  const wMeta = [{id : 1, info : "TRADITIONAL RAJASHTANI", price : "Rs 4999"}, {id : 2, info : "PUNJABI OVERSIZED LEHNGA", price : "Rs 3999"}, {id : 3, info : "ANARKALI KURTA INDIAN", price : "Rs 2999"}, {id : 4, info : "LEHNGA CHOLI WESTERN", price : "Rs 6999"}, {id : 5, info : "HRX OVERSIZED LIMITED EDI", price : "Rs 3999"}, {id : 6, info : "ANARKALI KURTI EXLUSIVE", price : "Rs 4999"}, {id : 7, info : "TRADITIONAL RAJASHTANI ", price : "Rs 3999"}, {id : 8, info : "TRADITIONAL INDIAN STYLE", price : "Rs 5999"}];
   const meta = [{id : 1, info : "PUMA OVERSIZED T-SHIRTS", price : "Rs 999"}, {id : 2, info : "NIKE OVERSIZED T-SHIRTS", price : "Rs 1999"}, {id : 3, info : "JORDAN OVERSIZED T-SHIRTS", price : "Rs 2999"}, {id : 4, info : "NBM OVERSIZED T-SHIRTS", price : "Rs 1999"}, {id : 5, info : "HRX OVERSIZED T-SHIRTS", price : "Rs 999"}, {id : 6, info : "BEING HUMAN T-SHIRTS", price : "Rs 999"}, {id : 7, info : "ADIDAS OVERSIZED T-SHIRTS", price : "Rs 3999"}, {id : 8, info : "PUMA OVERSIZED T-SHIRTS", price : "Rs 1000"}];
   var ind = 0;
   var indW = 0;
